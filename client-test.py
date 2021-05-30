dab339d6d9436c084fea3cf37b24a07e1051980c
kunst1080 <kontrapunkt1080@gmail.com>
2021-05-30 06:28:22 +0000

    fix for test

--- client-test.py
+++ client-test.py
@@ -1,8 +1,12 @@
 import asyncio
 import websockets
+import os
+
+address = os.getenv("WS_HOST", "127.0.0.1")
+port = os.getenv("WS_PORT", "1111")
 
 async def hello():
-    uri = "ws://127.0.0.1:1111"
+    uri = f"ws://{address}:{port}"
     async with websockets.connect(uri) as websocket:
         r = await websocket.recv()
         print(r)
